46750cb590a28fe5bd7f044d6dcdc23a9f14f91f
imalshi@sits.lk <imalshi@sits.lk>
2022-05-15 23:00:34 +0530

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World from imalshi! V2')
+  res.send('Hello World from imalshi! V DOCKER2')
 })
 
 app.listen(port, () => {
